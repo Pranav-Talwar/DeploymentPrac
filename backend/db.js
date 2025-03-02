f099ce696834c5e872b65c7d59552fca7e703b51
user.email <talwarpranav929@gmail.com>
2025-03-02 16:47:08 -0700

    Initial commit for DeploymentPrac

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://localhost:27017/iNotebook";
+const connectToMongo = async () => {
+  await mongoose
+    .connect(mongoURI)
+    .then(() => console.log("Connected to Mongo Successfully"))
+    .catch((err) => console.log(err));
+};
+
+module.exports = connectToMongo;
+
